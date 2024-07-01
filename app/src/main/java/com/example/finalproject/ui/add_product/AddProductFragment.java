b86f126457d0b14594fba86234f17d8db1862a13
yakir3160 <132565923+yakir3160@users.noreply.github.com>
2024-07-01 09:44:07 +0300

    - Created the view for AddProductFragment

--- app/src/main/java/com/example/finalproject/ui/add_product/AddProductFragment.java
+++ app/src/main/java/com/example/finalproject/ui/add_product/AddProductFragment.java
@@ -5,27 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.finalproject.R;
 import com.example.finalproject.databinding.FragmentAddProductBinding;
 
 public class AddProductFragment extends Fragment {
 
     private FragmentAddProductBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AddProductViewModel predefinedListsViewModel =
+        AddProductViewModel addProductViewModel =
                 new ViewModelProvider(this).get(AddProductViewModel.class);
 
         binding = FragmentAddProductBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        // Set up the spinner
+        Spinner spinner = binding.spinner;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        final TextView textView = binding.textAddProduct;
-        predefinedListsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
